e39cebcf5ee12e34b9a360d7dcf110c07a19b3d0
Bharath_G_S <bharath.g.s115@gmail.com>
2025-04-21 12:39:43 +0530

    counter project done

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // let counter = 15
+
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () =>{
+    if(counter < 20){
+      setCounter(counter + 1)
+    }else{
+      return
+    }
+
+
+     
+  }
+
+  function decreaseValue() {
+    if (counter <= 0){
+      return
+    }
+    else{
+      setCounter(counter-1)
+    }
+  }
+
+  function clear(){
+    setCounter(counter - counter)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>This is counter Project....!</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quaerat veniam tempore ab obcaecati. Minima facere officiis quos dignissimos aliquam accusamus animi, blanditiis inventore veniam. Temporibus accusamus itaque minima aspernatur.
+        {counter}
       </p>
+
+      <button onClick={addValue}>Add value</button> <br /><br />
+      <button onClick={decreaseValue}>Decrese Value</button>
+      <button onClick={clear}>clear Value</button>
     </>
   )
 }
