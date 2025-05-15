3cbbaa9e35d09a2dabcb0a3efa22fa80bb31897e
Bharath_G_S <bharath.g.s115@gmail.com>
2025-05-15 20:24:28 +0530

    React_Router_dom

---
+++ ReactRouterDom/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    <Home/>
+    
+    </>
+  )
+}
+
+export default App
